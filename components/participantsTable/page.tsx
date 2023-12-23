5f50dc0fe784f56a54af07968df6411572bee746
Srivatsa <srivatsarupadhya@gmail.com>
2023-12-23 14:02:09 +0530

    fix: verify button

--- components/participantsTable/page.tsx
+++ components/participantsTable/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { getTeamsList, verifyUser } from "@/app/_actions";
 import { College, Team, User } from "@prisma/client";
 import Image from "next/image";
@@ -11,10 +11,10 @@ import {
 	TableHeader,
 } from "react-aria-components";
 
-type members = User & {college:College | null}
-type data = Team & {members:members[]};
+type members = User & { college: College | null };
+type data = Team & { members: members[] };
 
-export default function ParticipantsTable({ data }: { data: data[]}) {
+export default function ParticipantsTable({ data }: { data: data[] }) {
 	return (
 		<>
 			<Table className="m-auto w-full" aria-label="Files">
@@ -33,54 +33,74 @@ export default function ParticipantsTable({ data }: { data: data[]}) {
 					{data.map((element) => {
 						return element.members.map((member, index) => {
 							return (
-								<Row key={index}>
-									<Cell className={"text-center border p-4"}>
-										{member.name}
-									</Cell>
-									<Cell className={"text-center border p-4"}>
-										{member.college?.name}
-									</Cell>
-									<Cell className={"text-center border p-4"}>
-										<Image
-											src={
-												member.college_id
-													? member.college_id
-													: ""
-											}
-											alt="ID"
-											width="100"
-											height="100"
-										/>
-									</Cell>
-									<Cell className={"text-center border p-4"}>
-										<Image
-											src={
-												member.adhaar
-													? member.adhaar
-													: ""
-											}
-											alt="Adhaar"
-											width="100"
-											height="100"
-										/>
-									</Cell>
-									<Cell className={"text-center border p-4"}>
-										{member.paymentStatus}
-									</Cell>
-									<Cell className={"text-center border p-4"}>
-										<button
-											data-uid={member.id}
-											onClick={(e) =>
-												verifyUser(
-													e.target.getAttribute("uid")
-												)
-											}
-											className="bg-white p-4 rounded text-black"
+								<>
+									<Row className={"border p-2 text-xl"}>
+										<Cell>Team: {element.name}</Cell>
+									</Row>
+									<Row key={index}>
+										<Cell
+											className={"text-center border p-4"}
 										>
-											Verify
-										</button>
-									</Cell>
-								</Row>
+											{member.name}
+										</Cell>
+										<Cell
+											className={"text-center border p-4"}
+										>
+											{member.college?.name}
+										</Cell>
+										<Cell
+											className={"text-center border p-4"}
+										>
+											<Image
+												src={
+													member.college_id
+														? member.college_id
+														: ""
+												}
+												alt="ID"
+												width="100"
+												height="100"
+											/>
+										</Cell>
+										<Cell
+											className={"text-center border p-4"}
+										>
+											<Image
+												src={
+													member.adhaar
+														? member.adhaar
+														: ""
+												}
+												alt="Adhaar"
+												width="100"
+												height="100"
+											/>
+										</Cell>
+										<Cell
+											className={"text-center border p-4"}
+										>
+											{member.paymentStatus}
+										</Cell>
+										<Cell
+											className={"text-center border p-4"}
+										>
+											<button
+												data-uid={member.id}
+												onClick={(e) => {
+													const id =
+														e.currentTarget.getAttribute(
+															"data-uid"
+														);
+														console.log(id)
+													if (id) verifyUser(id);
+												}}
+												className="bg-white p-4 rounded text-black"
+											>
+												Verify
+											</button>
+										</Cell>
+									</Row>
+								</>
 							);
 						});
 					})}
