f251f03056ff496cd97c3189f204102207c08d24
nagarajpandith <nagaraj.pandith2002@gmail.com>
2024-01-04 21:13:57 +0530

    fix: text-shadow warning

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      textShadow: {
-        sm: '0 1px 2px var(--tw-shadow-color)',
-        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-        lg: '0 8px 16px var(--tw-shadow-color)',
-      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -102,16 +97,6 @@ const config: Config = {
     },
   },
   plugins: [
-    plugin(function ({ matchUtilities, theme }) {
-      matchUtilities(
-        {
-          'text-shadow': (value) => ({
-            textShadow: value,
-          }),
-        },
-        { values: theme('textShadow') }
-      );
-    }),
     plugin(function ({ addUtilities }) {
       addUtilities({
         '.rotate-x-20': {
