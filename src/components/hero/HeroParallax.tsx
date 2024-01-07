ed5e07be9643766c2aaa6db90dc25a9b9b65868e
nagarajpandith <nagaraj.pandith2002@gmail.com>
2024-01-07 14:24:36 +0530

    fix: positioning issues, scale down on scroll

--- src/components/hero/HeroParallax.tsx
+++ src/components/hero/HeroParallax.tsx
@@ -3,15 +3,14 @@ import Image from 'next/image';
 import HeroForeground from '@/public/images/hero-foreground2.svg';
 import HeroBackground from '@/public/images/hero-background.svg';
 import HackfestFont from '@/public/images/hackfest-text.png';
-import { MouseEvent, useRef, useState } from 'react';
-import { useScroll, useTransform, motion } from 'framer-motion';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
+import { useScroll, useTransform, motion, useSpring } from 'framer-motion';
 import { Alignment, Fit, Layout, useRive } from '@rive-app/react-canvas';
 
 const HeroParallax = () => {
   const ref = useRef(null);
   const { RiveComponent: Synthwave } = useRive({
-    src: `/rive/synthwave2.riv/`,
-    // stateMachines: ['state-machine'],
+    src: `/rive/synthwave.riv/`,
     autoplay: true,
     layout: new Layout({
       fit: Fit.Cover,
@@ -21,7 +20,6 @@ const HeroParallax = () => {
 
   const { RiveComponent: Hoverboard } = useRive({
     src: `/rive/hoverboard.riv/`,
-    // stateMachines: ['state-machine'],
     autoplay: true,
     layout: new Layout({
       fit: Fit.FitHeight,
@@ -34,10 +32,17 @@ const HeroParallax = () => {
     offset: ['start end', 'end start'],
   });
 
-  const bgSpeed = useTransform(scrollYProgress, [0, 1], [-150, 200]);
-  const textSpeed = useTransform(scrollYProgress, [0, 1], [-300, 300]);
-  const fgSpeed = useTransform(scrollYProgress, [0, 1], [-150, 150]);
-  const swSpeed = useTransform(scrollYProgress, [0, 1], [-130, 130]);
+  const smoothScroll = useSpring(scrollYProgress, {
+    mass: 0.5,
+    damping: 25,
+    stiffness: 150,
+  });
+
+  const textSpeed = useTransform(smoothScroll, [0, 1], [-300, 300]);
+  const fgSpeed = useTransform(smoothScroll, [0, 1], [-150, 150]);
+
+  const textScale = useTransform(smoothScroll, [0, 1], [1.5, 0.5]);
+
   const [x, setX] = useState(0);
 
   const handleMouseMove = (e: MouseEvent) => {
@@ -52,34 +57,34 @@ const HeroParallax = () => {
       ref={ref}
     >
       <motion.div
-        style={{ y: textSpeed }}
-        className="absolute inset-0 z-30 flex justify-center items-center"
+        style={{ y: textSpeed, scale: textScale }}
+        className="absolute inset-0 z-10 flex justify-center items-center"
       >
-        <Image className="w-[800px]" src={HackfestFont} alt="Hackfest Font" />
+        <Image className={`w-[800px]`} src={HackfestFont} alt="Hackfest Font" />
       </motion.div>
 
       <motion.div
         style={{ y: fgSpeed }}
-        className="h-2/3 -mb-2 mt-2  w-screen "
+        className="-z-20 h-2/3 -mb-2 mt-2 w-screen relative"
       >
         <Image
           src={HeroForeground}
           alt="Hero Foreground"
-          className="object-cover -z-20  w-full h-full object-bottom "
+          className="object-cover w-full h-full object-bottom"
         />
       </motion.div>
 
-      <motion.div style={{ y: swSpeed }} className="z-30 h-1/3  w-screen ">
-        <Synthwave className="z-10 w-full h-full" />
-      </motion.div>
+      <div className="z-20 absolute h-1/3 w-screen">
+        <Synthwave className="w-full h-full" />
+      </div>
 
-      <motion.div style={{ y: bgSpeed }} className="absolute -z-10 inset-0">
+      <div className="absolute -z-30 inset-0">
         <Image
           src={HeroBackground}
           alt="Hero Background"
           className="h-screen w-screen object-cover object-top"
         />
-      </motion.div>
+      </div>
 
       <div
         className={`z-40 absolute h-[30rem] w-[30rem] bottom-0`}
