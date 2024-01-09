b8bab81e7fd38b1bc19e3f918ffb0556a1778ec4
satwikrprabhu <satwikprabhu@gmail.com>
2024-01-10 01:09:11 +0530

    feat: lamp effect

--- src/components/prizePool/index.tsx
+++ src/components/prizePool/index.tsx
@@ -2,10 +2,10 @@
 import Image from 'next/image';
 import Confetti from 'react-dom-confetti';
 import { useEffect, useState } from 'react';
-import { Meteors } from './meteors';
 import { useRef } from 'react';
 import { useInView } from 'framer-motion';
-
+import { Lamp,LampContainer } from './lamp';
+import {motion} from 'framer-motion';
 const PrizePool = () => {
   const ref = useRef(null);
   const isInView = useInView(ref);
@@ -18,7 +18,7 @@ const PrizePool = () => {
     angle: 290,
     spread: 360,
     startVelocity: 40,
-    elementCount: 150,
+    elementCount: 50,
     dragFriction: 0.11,
     duration: 3020,
     stagger: 3,
@@ -50,29 +50,27 @@ const PrizePool = () => {
   };
 
   return (
-    <>
-      <div className="min-h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-        <div className="w-full absolute inset-0 h-screen">
-          {/* <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.4}
-          particleDensity={100}
-          className="w-full h-full"
-          particleColor="#FFFFFF"
-        /> */}
-          {/* <Meteors number={100} /> */}
+    <LampContainer>
+      <motion.div className="min-h-screen relative w-full  flex flex-col items-center justify-center rounded-md py-20  "
+      initial={{ opacity: 0.5, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.3,
+        duration: 0.8,
+        ease: "easeInOut",
+      }}
+      >
+        <div className="w-full absolute inset-0 ">
         </div>
         <div className="flex flex-col  items-center p-12 space-y-16">
           <div>
             <h1 className="text-5xl font-bold">Prize Pool</h1>
             <Image
-              src="/images/ufo.png"
+              src="/images/airship.png"
               width={250}
               height={200}
               alt="UFO"
-              className="py-8 fly-up-down ease-in-out drop-shadow-xl z-50"
+              className="fly-up-down ease-in-out drop-shadow-xl z-50"
               onClick={() => ConfettiExplosion()}
             />
           </div>
@@ -85,7 +83,7 @@ const PrizePool = () => {
         >
           {/* Iterate cards */}
           <div className="relative">
-            <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-90 bg-red-500 rounded-full blur-3xl" />
+            {/* <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-90 bg-red-500 rounded-full blur-3xl" /> */}
             <div className="relative shadow-xl bg-gradient-to-r from-base-800 via-base-700 to-base-800 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-center items-center">
               <h1 className="font-bold text-2xl text-white mb-4 relative z-50">
                 Runner Up
@@ -96,12 +94,10 @@ const PrizePool = () => {
               <p className="font-normal text-base text-slate-500 mb-4 z-50 text-center ">
                 Goodies + Swags + Certificates + Internship Opportunities
               </p>
-              {/* Meaty part - Meteor effect */}
-              <Meteors number={20} />
             </div>
           </div>
           <div className="relative">
-            <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-90 bg-red-500 rounded-full blur-3xl" />
+            {/* <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-90 bg-red-500 rounded-full blur-3xl" /> */}
             <div className="relative shadow-xl bg-gradient-to-r from-base-800 via-base-700 to-base-800 border border-gray-800  px-4 py-12 h-full overflow-hidden rounded-2xl flex flex-col justify-center items-center">
               <h1 className="font-bold text-2xl text-white mb-4 relative z-50">
                 Winner
@@ -112,12 +108,10 @@ const PrizePool = () => {
               <p className="font-normal text-base text-slate-500 mb-4 z-50 text-center ">
                 Goodies + Swags + Certificates + Internship Opportunities
               </p>
-              {/* Meaty part - Meteor effect */}
-              <Meteors number={20} />
             </div>
           </div>
           <div className="relative">
-            <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-90 bg-red-500 rounded-full blur-3xl" />
+            {/* <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-90 bg-red-500 rounded-full blur-3xl" /> */}
             <div className="relative shadow-xl bg-gradient-to-r from-base-800 via-base-700 to-base-800 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-center items-center">
               <h1 className="font-bold text-2xl text-white mb-4 relative z-50">
                 2nd Runner Up
@@ -128,13 +122,12 @@ const PrizePool = () => {
               <p className="font-normal text-base text-slate-500 mb-4 z-50 text-center ">
                 Goodies + Swags + Certificates + Internship Opportunities
               </p>
-              {/* Meaty part - Meteor effect */}
-              <Meteors number={20} />
             </div>
           </div>
         </div>
-      </div>
-    </>
+        {/* <Lamp /> */}
+      </motion.div>
+    </LampContainer>
   );
 };
 
