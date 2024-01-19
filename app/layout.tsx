76410b471362bd29fc3e99947a8fa08d3ac831df
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2024-01-19 15:47:09 +0530

    fix: added head

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,19 @@ const metadata: Metadata = {
     "Hackfest is a 36h long National level Hackathon at NMAMIT, Nitte coupled with a lot of Interactive Activities & Tech Conference talks!",
 };
 
+const meta = {
+  title: `${
+    pathname === ""
+      ? ""
+      : pathname.charAt(1).toUpperCase() + pathname.slice(2) + " | "
+  }${siteMetadata.title}`,
+  description: siteMetadata.description,
+  type: "Website",
+  canonicalUrl: `${siteMetadata.siteUrl}${pathname}`,
+  isArticle: false,
+  ...customMeta,
+};
+
 export { metadata };
 
 export default function RootLayout({
@@ -25,6 +38,42 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>{meta.title}</title>
+        <meta content={meta.description} name="description" />
+        <link href="/favicons/favicon.ico" rel="shortcut icon" />
+        <link
+          href="/favicons/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/favicons/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/favicons/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#000000"></meta>
+        <meta
+          name="msapplication-square150x150logo"
+          content="/favicons/mstile-150x150.png"
+        />
+        <link rel="canonical" href={meta.canonicalUrl} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={"/logos/logo.png"} />
+        {/* TODO: OG Image API */}
+      </head>
       <body
         className={`${inter.className} ${font.variable} bg-black text-white`}
       >
