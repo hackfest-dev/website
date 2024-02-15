8dab4c42ea74f3b6b0b98d96fbd122d407aac33a
Keerthan N S <keerthanns091@gmail.com>
2024-02-15 06:35:32 +0530

    [fix]edit profile layout

--- src/app/(routes)/user/profile/page.tsx
+++ src/app/(routes)/user/profile/page.tsx
@@ -31,25 +31,13 @@ export default async function ProfilePage() {
     );
   return (
     <>
-      <div className="p-6 md:p-auto m-auto bg-white text-black min-h-screen md:flex self-center justify-evenly">
-        <div className="flex w-full fixed justify-center items-center mt-80 overflow-hidden -z-10">
-          <div className="grid grid-cols-2 gap-0 w-fit fixed opacity-80 mt-80 blur-sm">
-            <div className="bg-black border-base-300 shadow-[0_0px_60px_-15px_rgba(0,0,0,0.3)] shadow-base-200 w-32 h-32 md:w-72 md:h-72 border mt-8 ml-8 animate-[flicker_10s_ease_infinite_1s]"></div>
-            <div className="bg-black border-base-300 shadow-[0_0px_60px_-15px_rgba(0,0,0,0.3)] shadow-base-200 w-40 h-40 md:w-80 md:h-80 border animate-[flicker_16s_ease_infinite_3s]"></div>
-            <div className="bg-black border-base-300 shadow-[0_0px_60px_-15px_rgba(0,0,0,0.3)] shadow-base-200 w-40 h-40 md:w-80 md:h-80 border animate-flicker"></div>
-            <div className="bg-black border-base-300 shadow-[0_0px_60px_-15px_rgba(0,0,0,0.3)] shadow-base-200 w-32 h-32 md:w-72 md:h-72 border animate-[flicker_6s_ease_infinite_5s]"></div>
-          </div>
-        </div>
-        <div className="w-full lg:w-3/4 m-auto text-black h-full md:flex justify-between self-center">
-          <IdeaSubmitForm />
-          {userInfo && <Profile user={userInfo}></Profile>}
+      <div className="mt-24 flex flex-col justify-center md:flex-row gap-2 w-full min-h-screen p-3 md:px-auto mx-auto max-w-5xl">
+        {userInfo && <Profile user={userInfo}/>}
           {!user.team?.id ? (
             <CreateTeam />
           ) : (
             <TeamDetails teamid={user.team?.id} />
           )}
-          {/* <EditProfile /> */}
-        </div>
       </div>
     </>
   );
