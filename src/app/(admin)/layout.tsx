bdad9b0919ca6b20c872cfdb3e32e3f62f47dca1
nagarajpandith <nagaraj.pandith2002@gmail.com>
2024-02-17 14:42:05 +0530

    feat: custom 404 & admin layout fix

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,14 +1,29 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "@/src/app/globals.css";
-import { userInfo } from "@/src/lib/session";
-import FaqAdmin from "@/src/components/faq/faqAdmin";
+import { Poppins } from 'next/font/google';
+import '../globals.css';
+import localFont from 'next/font/local';
+import { Toaster } from 'sonner';
+import ProgressBarProvider from '@/src/components/progressBarProvider';
+import { Metadata } from 'next';
+import NotFound from '../(routes)/not-found';
+import FaqAdmin from '@/src/components/faq/faqAdmin';
+import { userInfo } from '@/src/lib/session';
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
+const obscura = localFont({
+  src: '../../../public/fonts/camera-obscura.otf',
+  display: 'swap',
+  variable: '--font-obscura',
+});
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
 const metadata: Metadata = {
-  title: "Hackfest - Admin Dashboard",
-  description: "Admin Dashboard",
+  title: 'Hackfest - Admin Dashboard',
+  description: 'Admin Dashboard',
+  icons: {
+    icon: '/favicons/favicon.ico',
+    shortcut: '/favicons/favicon.ico',
+    apple: '/favicons/apple-touch-icon.png',
+  },
 };
 
 export { metadata };
@@ -20,24 +35,22 @@ export default async function RootLayout({
 }) {
   const user = await userInfo();
 
-  if (user !== null && user.role === "ADMIN") {
+  if (user !== null && user.role === 'ADMIN') {
     return (
-      <html lang="en">
-        <body className={`${poppins.className} bg-black text-white`}>
-          {children}
-          <div className="sticky bottom-5 ml-5">
-            <FaqAdmin />
-          </div>
+      <html lang="en" className={`${obscura.variable}`}>
+        <body className={`dark bg-black text-white ${poppins.className}`}>
+          <ProgressBarProvider>
+            <Toaster richColors expand={true} position="top-center" />
+
+            {children}
+            <div className="sticky bottom-5 ml-5">
+              <FaqAdmin />
+            </div>
+          </ProgressBarProvider>
         </body>
       </html>
     );
   } else {
-    return (
-      <html lang="en">
-        <body className={`${poppins.className} bg-black text-white`}>
-          <h1>Unauthorized</h1>
-        </body>
-      </html>
-    );
+    return <NotFound />;
   }
 }
