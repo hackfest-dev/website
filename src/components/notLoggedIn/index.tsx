176ac4e5eff58cb6b0638b54fdc755e2f86a10ba
nagarajpandith <nagaraj.pandith2002@gmail.com>
2024-02-19 20:04:54 +0530

    fix: HF logo

--- src/components/notLoggedIn/index.tsx
+++ src/components/notLoggedIn/index.tsx
@@ -15,11 +15,20 @@ const NotLoggedIn = () => {
         <>
           <div className="z-10 bg-black/50 py-10 px-5 flex justify-center items-center flex-col border border-white/20 w-full rounded-3xl">
             <div className="text-center flex justify-center flex-col">
+              <div className="relative w-full h-full group flex justify-center items-center">
+                <Image
+                  src="/logos/logo.png"
+                  alt="Hackfest_Logo"
+                  width={180}
+                  height={180}
+                />
+                <div className="group-hover:brightness-125 transition-all duration-300 absolute h-full -z-10 w-full bg-gradient-radial from-yellow-300/50 via-transparent to-transparent blur-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+              </div>
               <h1 className="text-4xl md:text-7xl font-black text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
                 Hey there! 👋🏻
               </h1>
               <p className="mt-4 text-gray-300 mb-5 text-md">
-                Please sign in and you are all set to Register. Happy Coding!
+                Please sign in and you are all set to Register. Happy Coding.
               </p>
               <SignInButton />
             </div>
