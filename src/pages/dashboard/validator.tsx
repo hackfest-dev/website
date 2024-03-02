aea47de12f788f99ccb31b0726ec0dbf85918c6e
Nandan Pai <nandanrp09@gmail.com>
2024-03-03 01:52:54 +0530

    Valiator Dashboard

--- src/pages/dashboard/validator.tsx
+++ src/pages/dashboard/validator.tsx
@@ -7,12 +7,6 @@ import {
   TableCell,
 } from "~/components/ui/table";
 import { api } from "~/utils/api";
-import { Button } from "~/components/ui/button";
-import { useState } from "react";
-import { inferRouterOutputs } from "@trpc/server";
-import { teamRouter } from "~/server/api/routers/team";
-import { Scores, Team, Tracks, User } from "@prisma/client";
-import { Dialog, DialogTrigger, DialogContent } from "~/components/ui/dialog";
 import DashboardLayout from "~/components/layout/dashboardLayout";
 import { useSession } from "next-auth/react";
 import { toast } from "sonner";
@@ -23,9 +17,10 @@ import PDFModal from "~/components/validatorDashboard/pdfModal";
 
 export default function Validator() {
   const submitScore = api.validator.setScore.useMutation({
-    onSuccess: () => {
+    onSuccess: async () => {
+      toast.dismiss('submitting-score');
       toast.success('Score submitted');
-      teamData.refetch()
+      await teamData.refetch()
     },
     onError: (error) => {
       toast.error(error.message);
@@ -34,12 +29,25 @@ export default function Validator() {
   const teamData = api.team.getTeamsList.useQuery();
   const user = useSession();
 
+  if(submitScore.isLoading){
+    toast.loading('Submitting score',{id: 'submitting-score'});
+  }
   
 
   
 
   return (
     !teamData.isLoading && <DashboardLayout>
+      <div className="flex flex-col gap-6 w-full py-4 justify-center items-center">
+        <h1 className="md:text-5xl text-3xl font-semibold">Validator Dashboard</h1>
+        <div className="flex flex-col gap-y-2 justify-center w-full items-center">
+          <span className="text-xl">Hey there {user?.data?.user.name} 👋</span>
+          <ul className="flex flex-col list-disc text-base text-slate-500">
+            <li>You can cahnge your scores for a team</li>
+            <li>Once you submit score for a team, the button turns green</li>
+          </ul>
+        </div>
+      </div>
       <div className="rounded-md border">
       
       <Table>
@@ -54,7 +62,10 @@ export default function Validator() {
             </TableHead>
             <TableHead>
               PPT
-            </TableHead> 
+            </TableHead>
+            <TableHead>
+            </TableHead>
+            <TableHead>Scores</TableHead> 
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -75,8 +86,8 @@ export default function Validator() {
                     <TableCell>
                     <button
                       className={`${team?.Scores[0]?.userId === user?.data?.user.id && team?.Scores[0]?.score.score === '5' ? 'bg-green-700 text-white' : 'bg-white text-black'} px-4 py-2 rounded-lg` }
-                      onClick={() => {
-                        submitScore.mutateAsync({
+                      onClick={async () => {
+                        await submitScore.mutateAsync({
                           teamId: team.id,
                           score: '5',
                         });
@@ -90,8 +101,8 @@ export default function Validator() {
                     <button
             
             className={`${team?.Scores[0]?.userId === user?.data?.user.id && team?.Scores[0]?.score.score === '10' ? 'bg-green-700 text-white' : 'bg-white text-black'} px-4 py-2 rounded-lg` }
-              onClick={() => {
-                submitScore.mutateAsync({
+              onClick={async () => {
+                await submitScore.mutateAsync({
                   teamId: team.id,
                   score: '10',
                 });
@@ -105,8 +116,8 @@ export default function Validator() {
                     <button
             
             className={`${team?.Scores[0]?.userId === user?.data?.user.id && team?.Scores[0]?.score.score === '15' ? 'bg-green-700 text-white' : 'bg-white text-black'} px-4 py-2 rounded-lg` }
-              onClick={() => {
-                submitScore.mutateAsync({
+              onClick={async () => {
+                await submitScore.mutateAsync({
                   teamId: team.id,
                   score: '15',
                 });
