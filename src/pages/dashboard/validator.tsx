2fb25a202c0d7d03674e4b2c45bb991bef84b430
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2024-03-02 18:45:56 +0530

    feat: validator init

---
+++ src/pages/dashboard/validator.tsx
@@ -0,0 +1,165 @@
+import {
+  TableHeader,
+  TableRow,
+  Table,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "~/components/ui/table";
+import { api } from "~/utils/api";
+
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
+  type ColumnDef,
+  type VisibilityState,
+  type SortingState,
+  type ColumnFiltersState,
+} from "@tanstack/react-table";
+import { Button } from "~/components/ui/button";
+import { useState } from "react";
+import { inferRouterOutputs } from "@trpc/server";
+import { teamRouter } from "~/server/api/routers/team";
+import { Tracks } from "@prisma/client";
+
+interface SubmissionRow {
+  ideaSubmission: { track: Tracks; pptUrl: string };
+}
+
+export default function Validator() {
+  const teamData = api.team.getTeamsList.useQuery().data;
+
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+
+  const columns: ColumnDef<
+    unknown,
+    inferRouterOutputs<typeof teamRouter>["getTeamsList"]
+  >[] = [
+    {
+      accessorKey: "name",
+      header: "Team Name",
+    },
+    {
+      accessorKey: "ideaSubmission",
+      header: "PPT",
+      cell: (cell) => {
+        (cell.cell.row.original as SubmissionRow).ideaSubmission ? "yes" : "no";
+      },
+    },
+  ];
+
+  const table = useReactTable({
+    data: teamData ?? [],
+    columns: columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    // onRowSelectionChange: getSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="rounded-md border">
+      {/* <Input
+          placeholder="Search teams"
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        /> */}
+
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.firstPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<<"}
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<"}
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {">"}
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.lastPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {">>"}
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/server/api/routers/team.ts
+++ src/server/api/routers/team.ts
@@ -237,7 +237,7 @@ export const teamRouter = createTRPCRouter({
   deleteTeam: protectedProcedure.mutation(async ({ ctx }) => {
     try {
       const user = ctx.session.user;
-      
+
       if (!user?.isLeader) {
         throw new TRPCError({
           code: "BAD_REQUEST",
@@ -306,8 +306,11 @@ export const teamRouter = createTRPCRouter({
     }),
 
   getTeamsList: protectedProcedure.query(async ({ ctx }) => {
-    console.log(ctx.session.user.role)
-    if (ctx.session.user.role !== 'ORGANISER')
+    console.log(ctx.session.user.role);
+    if (
+      ctx.session.user.role !== "ORGANISER" &&
+      ctx.session.user.role !== "VALIDATOR"
+    )
       throw new TRPCError({
         code: "FORBIDDEN",
         message: "You are not an organiser",
