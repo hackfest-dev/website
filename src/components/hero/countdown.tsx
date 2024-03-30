f9fc74589a9fe3fd06832cbd9c412a8a73357425
Nandan R Pai <nandanrp09@gmail.com>
2024-03-30 08:16:59 +0530

    Fix timer

--- src/components/hero/countdown.tsx
+++ src/components/hero/countdown.tsx
@@ -1,11 +1,25 @@
 import { useEffect, useState } from "react";
 
-export default function Countown({ eventTime }: { eventTime: Date }) {
+export default function Countown() {
+  const eventTime = new Date("2022-10-15T00:00:00");
   const eventTimeSec = eventTime.getTime();
 
-  function remainingTime() {
-    const currentTime = new Date().getTime();
-    const remainingTime = eventTimeSec - currentTime;
+  const [time, setTime] = useState<{
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  function getRemaingTime() {
+    const eventDate = new Date("2024-04-05T10:00:00").getTime();
+    const currentDate = new Date().getTime();
+    const remainingTime = eventDate - currentDate;
 
     const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
@@ -16,20 +30,16 @@ export default function Countown({ eventTime }: { eventTime: Date }) {
     );
     const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-    return { days, hours, minutes, seconds };
+    setTime({ days, hours, minutes, seconds });
   }
-
-  const [time, setTime] = useState({
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  });
-
+  
   useEffect(() => {
-    setInterval(() => {
-      setTime(remainingTime());
+    getRemaingTime();
+    const interval = setInterval(() => {
+      getRemaingTime();
     }, 1000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
