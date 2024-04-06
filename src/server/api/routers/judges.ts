68094a23e26682dd3a50684d39083daa621fbe0f
Nandan R Pai <nandanrp09@gmail.com>
2024-04-07 03:37:11 +0530

    Fix

--- src/server/api/routers/judges.ts
+++ src/server/api/routers/judges.ts
@@ -450,31 +450,29 @@ export const JudgeRouter = createTRPCRouter({
         // get all criterias
         // get all scores by judge
 
-        await Promise.all(
-          scores.map(async (s) => {
-            const teamI = await ctx.db.team.findUnique({
-              where: {
-                id: s.teamId,
-              },
-            });
-  
-            const score =
-              (teamI?.JudgeTotalScore ?? 0) +
-              ((s.score - min) / (max - min)) * 10;
-            // console.log(s.score, min, max);
-            
-            await ctx.db.team.update({
-              where: {
-                id: s.teamId,
-              },
-              data: {
-                JudgeTotalScore: score,
-              },
-            });
-  
-            // console.log(score - Math.floor(score) > 0.5 ? Math.ceil(score) : Math.floor(score),score);
-          })
-        )
+        scores.map(async (s) => {
+          const teamI = await ctx.db.team.findUnique({
+            where: {
+              id: s.teamId,
+            },
+          });
+
+          const score =
+            (teamI?.JudgeTotalScore ?? 0) +
+            ((s.score - min) / (max - min)) * 10;
+          // console.log(s.score, min, max);
+          
+          await ctx.db.team.update({
+            where: {
+              id: s.teamId,
+            },
+            data: {
+              JudgeTotalScore: score,
+            },
+          });
+
+          // console.log(score - Math.floor(score) > 0.5 ? Math.ceil(score) : Math.floor(score),score);
+        })
         
       })
 
