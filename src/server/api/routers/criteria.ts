61891500fc914db2f8b844a0868558c1bd97ee70
satwikrprabhu <satwikprabhu@gmail.com>
2024-04-06 10:17:26 +0530

    fix criterias

--- src/server/api/routers/criteria.ts
+++ src/server/api/routers/criteria.ts
@@ -1,9 +1,16 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import z from "zod";
+import { CriteriaType } from "@prisma/client";
 
 export const criteraRouter = createTRPCRouter({
   //TODO: get criteria based on type if they are different
-  getCriteria: protectedProcedure.query(async ({ ctx }) => {
+  getCriteria: protectedProcedure.input(z.object({
+    type: z.nativeEnum(CriteriaType).optional()
+  })).query(async ({ ctx, input }) => {
     return await ctx.db.criteria.findMany({
+      where: {
+        type: input.type ?? "JUDGE"
+      },
       select: {
         id: true,
         name: true,
